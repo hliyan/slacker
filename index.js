0fd1071f9d31673f4e1b22b6c6437a6b4b9ac8f7
Hasitha Liyanage <hasithaliyan@gmail.com>
2020-06-07 18:17:00 +0530

    minor change to the API

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ expect(mockSend).toHaveBeenCalledWith({message: 'Hi, foo', username: 'foo'});
 
 // V2: event driven was overkill, try simple awaits like everyone else
 
-let res = await engine.say({username: 'foo', message: 'Hi, foo'});
-expect(res).toEqual({message: 'Hi, john', username: 'foo'});
+let res = await engine.say({username: 'john', text: 'hello'});
+expect(res).toEqual({username: 'john', text: 'Hi, john', });
 
 */
