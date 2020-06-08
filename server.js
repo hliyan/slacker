9ec80432a34358f8a57cac04685f3cdc58398ff1
Hasitha Liyanage <hasithaliyan@gmail.com>
2020-06-08 21:24:44 +0530

    added /slack/event endpoint with a basic challenge response

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // basic test endpoint
   res.status(200).json({hello: 'world'});
+});
+
+app.post('/slack/event', (req, res) => {
+  if (req.body.type === 'url_verification') { // challenge from slack
+    res.status(200).json({challenge: req.body.challenge});
+    return;
+  }
+
+  res.status(200).json({});
 });
 
 module.exports = app;
--- server.test.js
+++ server.test.js
@@ -6,6 +6,24 @@ describe('GET /', () => {
     request(app)
       .get('/')
       .expect('Content-Type', /json/)
-      .expect(200, {hello: 'world'}, done);
+      .expect(200, {hello: 'world'}, done)
+  });
+});
+
+describe('POST /slack/event', () => {
+  test('should respond to the challenge sent by slack', (done) => {
+    request(app)
+      .post('/slack/event')
+      .send({type: 'url_verification', challenge: '12345000'})
+      .set('Content-type', 'application/json')
+      .expect(200, {challenge: '12345000'}, done);
+  });
+
+  test('should respond with 200 for unknown requests', (done) => {
+    request(app)
+    .post('/slack/event')
+    .send({})
+    .set('Content-type', 'application/json')
+    .expect(200, {}, done);
   });
 });
