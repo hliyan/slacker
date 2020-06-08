d808ff06eb63cdec58563bad5c5f4975206ed93b
Hasitha Liyanage <hasithaliyan@gmail.com>
2020-06-08 18:48:16 +0530

    checking why heroko's process.env.PORT is not taking effect

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require('./server');
 
-server.listen(process.env.PORT | 3000, () => {
-  console.log('Listening...')
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log('Listening on ' + PORT);
 });
 
 
