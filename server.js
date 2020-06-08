bd26fea133d6713c70d75ffc0d58d3345bc26fb1
Hasitha Liyanage <hasithaliyan@gmail.com>
2020-06-08 21:50:37 +0530

    added todo: slack event signature verification

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ app.get('/', (req, res) => { // basic test endpoint
 });
 
 app.post('/slack/event', (req, res) => {
-  console.log(req.headers);
+  // TODO: https://api.slack.com/authentication/verifying-requests-from-slack
+  /*
+  if (!slack.isValidSignature(req.headers[x-slack-signature], 
+    req.headers['x-slack-request-timestamp'], req.body)) {
+      res.status(401).json({});
+      return;
+  }
+  */
+
+
   if (req.body.type === 'url_verification') { // challenge from slack
     res.status(200).json({challenge: req.body.challenge});
     return;
