cd597b11796ba73cb7cb81062742a3da276c5a41
Hasitha Liyanage <hasithaliyan@gmail.com>
2020-06-09 18:26:01 +0530

    testing slack signature calculation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const crypto = require('crypto');
 
 const app = express();
-app.use(express.text("*/*"));
+app.use(express.text({type: "*/*"}));
 
 app.get('/', (req, res) => { // basic test endpoint
   res.status(200).json({hello: 'world'});
