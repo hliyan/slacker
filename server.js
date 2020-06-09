6ac9000a273d852166a1e5e36034adcd15e22eb7
Hasitha Liyanage <hasithaliyan@gmail.com>
2020-06-09 16:56:41 +0530

    added sig verification to /slack/event for live testing with slack

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
+const crypto = require('crypto');
 
 const app = express();
 app.use(express.json());
+
+const sha256 = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET);
 
 app.get('/', (req, res) => { // basic test endpoint
   res.status(200).json({hello: 'world'});
 });
 
 app.post('/slack/event', (req, res) => {
-  // TODO: https://api.slack.com/authentication/verifying-requests-from-slack
-  /*
-  if (!slack.isValidSignature(req.headers[x-slack-signature], 
-    req.headers['x-slack-request-timestamp'], req.body)) {
-      res.status(401).json({});
-      return;
+  // https://api.slack.com/authentication/verifying-requests-from-slack
+  const signature = sha256.update(`v0:${ req.headers['x-slack-request-timestamp']}:${req.body}`);
+  if (req.headers['x-slack-signature'] !== signature) {
+    console.log('signature mismatch: ' + req.headers['x-slack-signature'] + ' != ' + signature);
+    res.status(401).json({});
+    return;    
   }
-  */
-
 
   if (req.body.type === 'url_verification') { // challenge from slack
     res.status(200).json({challenge: req.body.challenge});
